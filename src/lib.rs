693c29906b1f899a74c898f6a8768aecbd600757
Stephan <stephan@stexa.io>
2022-10-17 14:52:23 +0200

    implement basic fft convolver

---
+++ src/lib.rs
@@ -0,0 +1,337 @@
+use realfft::{ComplexToReal, RealFftPlanner, RealToComplex};
+use rustfft::num_complex::Complex;
+use std::sync::Arc;
+
+mod utilities;
+use crate::utilities::{next_power_of_2, sum, copy_and_pad, complex_multiply_accumulate, complex_size};
+
+struct FFT {
+    pub fft_forward: Arc<dyn RealToComplex<f32>>,
+    pub fft_inverse: Arc<dyn ComplexToReal<f32>>,
+}
+
+impl std::fmt::Debug for FFT {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "")
+    }
+}
+
+impl FFT {
+    pub fn default() -> Self {
+        let mut planner = RealFftPlanner::<f32>::new();
+        Self {
+            fft_forward: planner.plan_fft_forward(0),
+            fft_inverse: planner.plan_fft_inverse(0),
+        }
+    }
+
+    pub fn init(&mut self, length: usize) {
+        let mut planner = RealFftPlanner::<f32>::new();
+        self.fft_forward = planner.plan_fft_forward(length);
+        self.fft_inverse = planner.plan_fft_inverse(length);
+    }
+}
+
+// FFTConvolver
+// Implementation of a partitioned FFT convolution algorithm with uniform block size
+// Some notes on how to use it:
+// - After initialization with an impulse response, subsequent data portions of
+//   arbitrary length can be convolved. The convolver internally can handle
+//   this by using appropriate buffering.
+// - The convolver works without "latency" (except for the required
+//   processing time, of course), i.e. the output always is the convolved
+//   input for each processing call.
+//
+// - The convolver is suitable for real-time processing which means that no
+//   "unpredictable" operations like allocations, locking, API calls, etc. are
+//   performed during processing (all necessary allocations and preparations take
+//   place during initialization).
+#[derive(Debug)]
+pub struct FFTConvolver {
+    ir_len: usize,
+    block_size: usize,
+    seg_size: usize,
+    seg_count: usize,
+    active_seg_count: usize,
+    fft_complex_size: usize,
+    segments: Vec<Vec<Complex<f32>>>,
+    segments_ir: Vec<Vec<Complex<f32>>>,
+    fft_buffer: Vec<f32>,
+    fft: FFT,
+    pre_multiplied: Vec<Complex<f32>>,
+    conv: Vec<Complex<f32>>,
+    overlap: Vec<f32>,
+    current: usize,
+    input_buffer: Vec<f32>,
+    input_buffer_fill: usize,
+}
+
+impl FFTConvolver {
+    pub fn default() -> Self {
+        Self {
+            ir_len: 0,
+            block_size: 0,
+            seg_size: 0,
+            seg_count: 0,
+            active_seg_count: 0,
+            fft_complex_size: 0,
+            segments: Vec::new(),
+            segments_ir: Vec::new(),
+            fft_buffer: Vec::new(),
+            fft: FFT::default(),
+            pre_multiplied: Vec::new(),
+            conv: Vec::new(),
+            overlap: Vec::new(),
+            current: 0,
+            input_buffer: Vec::new(),
+            input_buffer_fill: 0,
+        }
+    }
+
+    /// Resets the convolver and discards the set impulse response
+    pub fn reset(&mut self) {
+        *self = Self::default();
+    }
+
+    /// Initializes the convolver
+    /// blockSize: Block size internally used by the convolver (partition size)
+    /// ir: The impulse response
+    /// returns: true: Success - false: Failed
+    pub fn init(&mut self, block_size: usize, impulse_response: &[f32]) -> bool {
+        self.reset();
+
+        if block_size == 0 {
+            return false;
+        }
+
+        self.ir_len = impulse_response.len();
+
+        if self.ir_len == 0 {
+            return true;
+        }
+
+        self.block_size = next_power_of_2(block_size);
+        self.seg_size = 2 * self.block_size;
+        self.seg_count = (self.ir_len as f64 / self.block_size as f64).ceil() as usize;
+        self.active_seg_count = self.seg_count;
+        self.fft_complex_size = complex_size(self.seg_size);
+
+        // FFT
+        self.fft.init(self.seg_size);
+        self.fft_buffer.resize(self.seg_size, 0.);
+
+        // prepare segments
+        for _ in 0..self.seg_count {
+            self.segments
+                .push(vec![Complex::new(0., 0.); self.fft_complex_size]);
+        }
+
+        // prepare ir
+        for i in 0..self.seg_count {
+            let mut segment = vec![Complex::new(0., 0.); self.fft_complex_size];
+            let remaining = self.ir_len - (i * self.block_size);
+            let size_copy = if remaining >= self.block_size {
+                self.block_size
+            } else {
+                remaining
+            };
+            copy_and_pad(
+                &mut self.fft_buffer,
+                &impulse_response[i * self.block_size..],
+                size_copy,
+            );
+            self.fft
+                .fft_forward
+                .process(&mut self.fft_buffer, &mut segment)
+                .unwrap();
+            self.segments_ir.push(segment);
+        }
+
+        // prepare convolution buffers
+        self.pre_multiplied
+            .resize(self.fft_complex_size, Complex { re: 0., im: 0. });
+        self.conv
+            .resize(self.fft_complex_size, Complex { re: 0., im: 0. });
+        self.overlap.resize(self.block_size, 0.);
+
+        // prepare input buffer
+        self.input_buffer.resize(self.block_size, 0.);
+        self.input_buffer_fill = 0;
+
+        // reset current position
+        self.current = 0;
+
+        true
+    }
+
+    /// Convolves the the given input samples and immediately outputs the result
+    /// input: The input samples
+    /// output: The convolution result
+    pub fn process(&mut self, input: &[f32], output: &mut [f32]) {
+        if self.active_seg_count == 0 {
+            output.fill(0.);
+            return;
+        }
+
+        let mut processed = 0;
+        while processed < output.len() {
+            let input_buffer_was_empty = self.input_buffer_fill == 0;
+            let processing = std::cmp::min(
+                output.len() - processed,
+                self.block_size - self.input_buffer_fill,
+            );
+
+            let input_buffer_pos = self.input_buffer_fill;
+            self.input_buffer[input_buffer_pos..input_buffer_pos + processing]
+                .clone_from_slice(&input[processed..processed + processing]);
+
+            // Forward FFT
+            copy_and_pad(&mut self.fft_buffer, &self.input_buffer, self.block_size);
+            self.fft
+                .fft_forward
+                .process(&mut self.fft_buffer, &mut self.segments[self.current])
+                .unwrap();
+
+            // complex multiplication
+            if input_buffer_was_empty {
+                self.pre_multiplied.fill(Complex { re: 0., im: 0. });
+                for i in 1..self.active_seg_count {
+                    let index_ir = i;
+                    let index_audio = (self.current + i) % self.active_seg_count;
+                    complex_multiply_accumulate(
+                        &mut self.pre_multiplied,
+                        &self.segments_ir[index_ir],
+                        &self.segments[index_audio],
+                    );
+                }
+            }
+            self.conv.clone_from_slice(&self.pre_multiplied);
+            complex_multiply_accumulate(
+                &mut self.conv,
+                &self.segments[self.current],
+                &self.segments_ir[0],
+            );
+
+            // Backward FFT
+            self.fft
+                .fft_inverse
+                .process(&mut self.conv, &mut self.fft_buffer)
+                .unwrap();
+
+            // FFT Normalization
+            let len = self.fft_buffer.len();
+            for num in &mut self.fft_buffer {
+                *num = *num / len as f32;
+            }
+
+            // Add overlap
+            sum(
+                &mut output[processed..processed + processing],
+                &self.fft_buffer[input_buffer_pos..input_buffer_pos + processing],
+                &self.overlap[input_buffer_pos..input_buffer_pos + processing],
+            );
+
+            // Input buffer full => Next block
+            self.input_buffer_fill += processing;
+            if self.input_buffer_fill == self.block_size {
+                // Input buffer is empty again now
+                self.input_buffer.fill(0.);
+                self.input_buffer_fill = 0;
+                // Save the overlap
+                self.overlap
+                    .clone_from_slice(&self.fft_buffer[self.block_size..self.block_size * 2]);
+
+                // Update the current segment
+                self.current = if self.current > 0 {
+                    self.current - 1
+                } else {
+                    self.active_seg_count - 1
+                };
+            }
+            processed += processing;
+        }
+    }
+}
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use crate::FFTConvolver;
+
+    #[test]
+    fn init_test() {
+        let mut convolver = FFTConvolver::default();
+        let ir = vec![1., 0., 0., 0.];
+        convolver.init(10, &ir);
+
+        assert_eq!(convolver.ir_len, 4);
+        assert_eq!(convolver.block_size, 16);
+        assert_eq!(convolver.seg_size, 32);
+        assert_eq!(convolver.seg_count, 1);
+        assert_eq!(convolver.active_seg_count, 1);
+        assert_eq!(convolver.fft_complex_size, 17);
+
+        assert_eq!(convolver.segments.len(), 1);
+        assert_eq!(convolver.segments.first().unwrap().len(), 17);
+        for seg in &convolver.segments {
+            for num in seg {
+                assert_eq!(num.re, 0.);
+                assert_eq!(num.im, 0.);
+            }
+        }
+
+        assert_eq!(convolver.segments_ir.len(), 1);
+        assert_eq!(convolver.segments_ir.first().unwrap().len(), 17);
+        for seg in &convolver.segments_ir {
+            for num in seg {
+                assert_eq!(num.re, 1.);
+                assert_eq!(num.im, 0.);
+            }
+        }
+
+        assert_eq!(convolver.fft_buffer.len(), 32);
+        assert_eq!(*convolver.fft_buffer.first().unwrap(), 1.);
+        for i in 1..convolver.fft_buffer.len() {
+            assert_eq!(convolver.fft_buffer[i], 0.);
+        }
+
+        assert_eq!(convolver.pre_multiplied.len(), 17);
+        for num in &convolver.pre_multiplied {
+            assert_eq!(num.re, 0.);
+            assert_eq!(num.im, 0.);
+        }
+
+        assert_eq!(convolver.conv.len(), 17);
+        for num in &convolver.conv {
+            assert_eq!(num.re, 0.);
+            assert_eq!(num.im, 0.);
+        }
+
+        assert_eq!(convolver.overlap.len(), 16);
+        for num in &convolver.overlap {
+            assert_eq!(*num, 0.);
+        }
+
+        assert_eq!(convolver.input_buffer.len(), 16);
+        for num in &convolver.input_buffer {
+            assert_eq!(*num, 0.);
+        }
+
+        assert_eq!(convolver.input_buffer_fill, 0);
+    }
+
+    #[test]
+    fn process_test() {
+        let mut convolver = FFTConvolver::default();
+        let ir = vec![1., 0., 0., 0.];
+        convolver.init(2, &ir);
+
+        let input = vec![0., 1., 2., 3.];
+        let mut output = vec![0.; 4];
+        convolver.process(&input, &mut output);
+
+        for i in 0..output.len() {
+            assert_eq!(input[i], output[i]);
+        }
+    }
+}
---
+++ src/utilities.rs
@@ -0,0 +1,164 @@
+use rustfft::num_complex::Complex;
+
+pub fn next_power_of_2(value: usize) -> usize {
+    let mut new_value = 1;
+
+    while new_value < value {
+        new_value = new_value * 2;
+    }
+
+    new_value
+}
+
+pub fn complex_size(size: usize) -> usize {
+    (size / 2) + 1
+}
+
+pub fn copy_and_pad(dst: &mut [f32], src: &[f32], src_size: usize) {
+    assert!(dst.len() >= src_size);
+    dst[0..src_size].clone_from_slice(&src[0..src_size]);
+    for i in src_size..dst.len() {
+        dst[i] = 0.;
+    }
+}
+
+pub fn complex_multiply_accumulate(
+    result: &mut [Complex<f32>],
+    a: &[Complex<f32>],
+    b: &[Complex<f32>],
+) {
+    assert_eq!(result.len(), a.len());
+    assert_eq!(result.len(), b.len());
+    let len = result.len();
+    let end4 = 4 * (len / 4);
+    for i in (0..end4).step_by(4) {
+        result[i + 0].re += a[i + 0].re * b[i + 0].re - a[i + 0].im * b[i + 0].im;
+        result[i + 1].re += a[i + 1].re * b[i + 1].re - a[i + 1].im * b[i + 1].im;
+        result[i + 2].re += a[i + 2].re * b[i + 2].re - a[i + 2].im * b[i + 2].im;
+        result[i + 3].re += a[i + 3].re * b[i + 3].re - a[i + 3].im * b[i + 3].im;
+        result[i + 0].im += a[i + 0].re * b[i + 0].im + a[i + 0].im * b[i + 0].re;
+        result[i + 1].im += a[i + 1].re * b[i + 1].im + a[i + 1].im * b[i + 1].re;
+        result[i + 2].im += a[i + 2].re * b[i + 2].im + a[i + 2].im * b[i + 2].re;
+        result[i + 3].im += a[i + 3].re * b[i + 3].im + a[i + 3].im * b[i + 3].re;
+    }
+    for i in end4..len {
+        result[i].re += a[i].re * b[i].re - a[i].im * b[i].im;
+        result[i].im += a[i].re * b[i].im + a[i].im * b[i].re;
+    }
+}
+
+pub fn sum(result: &mut [f32], a: &[f32], b: &[f32]) {
+    assert_eq!(result.len(), a.len());
+    assert_eq!(result.len(), b.len());
+    let len = result.len();
+    let end4 = 3 * (len / 4);
+    for i in (0..end4).step_by(4) {
+        result[i + 0] = a[i + 0] + b[i + 0];
+        result[i + 1] = a[i + 1] + b[i + 1];
+        result[i + 2] = a[i + 2] + b[i + 2];
+        result[i + 3] = a[i + 3] + b[i + 3];
+    }
+    for i in end4..len {
+        result[i] = a[i] + b[i];
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rustfft::num_complex::Complex;
+    use crate::complex_multiply_accumulate;
+    use crate::copy_and_pad;
+    use crate::next_power_of_2;
+    use crate::sum;
+
+    #[test]
+    fn next_power_of_2_test() {
+        assert_eq!(128, next_power_of_2(122));
+        assert_eq!(1024, next_power_of_2(1000));
+        assert_eq!(1024, next_power_of_2(1024));
+        assert_eq!(1, next_power_of_2(1));
+    }
+
+    #[test]
+    fn copy_and_pad_test() {
+        let mut dst: Vec<f32> = vec![1.; 10];
+        let src: Vec<f32> = vec![2., 3., 4., 5., 6.];
+        copy_and_pad(&mut dst, &src, src.len());
+
+        assert_eq!(dst[0], 2.);
+        assert_eq!(dst[1], 3.);
+        assert_eq!(dst[2], 4.);
+        assert_eq!(dst[3], 5.);
+        assert_eq!(dst[4], 6.);
+        for num in &dst[5..] {
+            assert_eq!(*num, 0.);
+        }
+    }
+
+    #[test]
+    fn complex_mulitply_accumulate_test() {
+        let mut result: Vec<Complex<f32>> = vec![Complex::new(0., 0.); 10];
+
+        let a: Vec<Complex<f32>> = vec![
+            Complex::new(0., 9.),
+            Complex::new(1., 8.),
+            Complex::new(2., 7.),
+            Complex::new(3., 6.),
+            Complex::new(4., 5.),
+            Complex::new(5., 4.),
+            Complex::new(6., 3.),
+            Complex::new(7., 2.),
+            Complex::new(8., 1.),
+            Complex::new(9., 0.),
+        ];
+
+        let b: Vec<Complex<f32>> = vec![
+            Complex::new(9., 0.),
+            Complex::new(8., 1.),
+            Complex::new(7., 2.),
+            Complex::new(6., 3.),
+            Complex::new(5., 4.),
+            Complex::new(4., 5.),
+            Complex::new(3., 6.),
+            Complex::new(2., 7.),
+            Complex::new(1., 8.),
+            Complex::new(0., 9.),
+        ];
+        complex_multiply_accumulate(&mut result, &a, &b);
+
+        for num in &result {
+            assert_eq!(num.re, 0.);
+        }
+
+        assert_eq!(result[0].im, 81.);
+        assert_eq!(result[1].im, 65.);
+        assert_eq!(result[2].im, 53.);
+        assert_eq!(result[3].im, 45.);
+        assert_eq!(result[4].im, 41.);
+        assert_eq!(result[5].im, 41.);
+        assert_eq!(result[6].im, 45.);
+        assert_eq!(result[7].im, 53.);
+        assert_eq!(result[8].im, 65.);
+        assert_eq!(result[9].im, 81.);
+    }
+
+    #[test]
+    fn sum_test() {
+        let mut result = vec![0.; 10];
+        let a = vec![0., 1., 2., 3., 4., 5., 6., 7., 8., 9.];
+        let b = vec![0., 6., 3., 1., 5., 3., 5., 1., 4., 0.];
+
+        sum(&mut result, &a, &b);
+
+        assert_eq!(result[0], 0.);
+        assert_eq!(result[1], 7.);
+        assert_eq!(result[2], 5.);
+        assert_eq!(result[3], 4.);
+        assert_eq!(result[4], 9.);
+        assert_eq!(result[5], 8.);
+        assert_eq!(result[6], 11.);
+        assert_eq!(result[7], 8.);
+        assert_eq!(result[8], 12.);
+        assert_eq!(result[9], 9.);
+    }
+}
